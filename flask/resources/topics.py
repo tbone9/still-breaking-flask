0788096ea8558f3ccdff6bc980eb6ca8168f7bb1
AnnaFinnerty <algfinnerty@gmail.com>
2019-11-19 16:22:29 -0700

    pulling fixed code

--- flask/resources/topics.py
+++ flask/resources/topics.py
@@ -7,3 +7,13 @@ topic = Blueprint('topics', 'topic')
 @topic.route('/', methods=['GET'])
 def get_all_topics():
     return 'Here are the topics!'
+
+#create topic
+@topic.route('/', methods=['POST'])
+def create_topic():
+    print('topic create route')
+    payload = request.get_json()
+    created_topic = models.Topic.create(**payload)
+    created_topic_dict = model_to_dict(create_topic)
+    return jsonify(status={'code': 201, 'msg': 'success'}, data=created_topic_dict)
+    
